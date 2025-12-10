3bdcfb2d3a959bc035b1d5b2fc6c017dd0306a19
Zenith004 <zenithdeepa@gmail.com>
2025-12-10 12:07:39 +0530

    A new hunt awaits

--- src/data/puzzlesData.js
+++ src/data/puzzlesData.js
@@ -1,12 +1,12 @@
 import { PUZZLE_LIBRARY } from "./puzzleLibrary";
 
 export const PUZZLES_DATA = [
-  { ...PUZZLE_LIBRARY[16] },
-  { ...PUZZLE_LIBRARY[17] },
-  { ...PUZZLE_LIBRARY[18] },
-  { ...PUZZLE_LIBRARY[19] },
-  { ...PUZZLE_LIBRARY[20] },
-  { ...PUZZLE_LIBRARY[21] },
+  // { ...PUZZLE_LIBRARY[16] },
+  // { ...PUZZLE_LIBRARY[17] },
+  // { ...PUZZLE_LIBRARY[18] },
+  // { ...PUZZLE_LIBRARY[19] },
+  // { ...PUZZLE_LIBRARY[20] },
+  // { ...PUZZLE_LIBRARY[21] },
   // { ...PUZZLE_LIBRARY[5] },
   // { ...PUZZLE_LIBRARY[6] },
   // { ...PUZZLE_LIBRARY[7] },
@@ -15,7 +15,11 @@ export const PUZZLES_DATA = [
   // { ...PUZZLE_LIBRARY[10] },
 ];
 
-for (let i = 0; i < PUZZLES_DATA.length - 1; i++) {
-  PUZZLES_DATA[i].nextId = PUZZLES_DATA[i + 1].id;
+if (PUZZLES_DATA.length > 0) {
+  for (let i = 0; i < PUZZLES_DATA.length - 1; i++) {
+    PUZZLES_DATA[i].nextId = PUZZLES_DATA[i + 1].id;
+  }
 }
-PUZZLES_DATA[PUZZLES_DATA.length - 1].nextId = null;
+if (PUZZLES_DATA.length > 0) {
+  PUZZLES_DATA[PUZZLES_DATA.length - 1].nextId = null;
+}
